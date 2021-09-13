267a7372103cfd92c64933aa63f93972018e6915
idrxptrey <hibber.trey@awatapu.school.nz>
2021-09-13 12:52:40 +1200

    Inital Commit

---
+++ 01_yes_no_v2.py
@@ -0,0 +1,38 @@
+
+
+# Functions goes here...
+def yes_no(question):
+    valid = false
+    while not valid:
+            response = input("Have you played this game"
+                              " before ?").lower()
+
+            if response == "yes" or response == "y":
+                response = "yes"
+
+
+            elif response == "no" or response == "n":
+                response = "no"
+
+             else: print("Please answer yes / no")
+
+
+# Main routine goes here...
+
+show_instructions = ""
+while show_instructions != "xxx":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game"
+                              " before ?").lower()
+    # If they say yes, output 'program continues'
+    # If they say no, output 'display instructions'
+    # If the answer is invalid, print an error.
+
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
+
+    elif show_instructions == "no" or show_instructions == "n":
+        print("display instructions")
+
+    else:
+        print("Please answer yes / no")
